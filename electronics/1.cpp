5a7b092c4e6c0c508ab2bf69011115d37e432a56
Harshita Sarraf <harshitasarraf123@gmail.com>
2024-11-28 11:22:37 +0530

    new

---
+++ electronics/1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class A {
+public:
+void print()
+{ cout <<" Inside A::"; }
+};
+class B : public A {
+public:
+void print()
+{ cout <<" Inside B"; }
+};
+class C: public B {
+};
+int main(void)
+{
+B c;
+c.print();
+return 0;
+}
